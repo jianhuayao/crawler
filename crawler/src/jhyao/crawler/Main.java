8f53c3dbca890fcbab0e1890643da6e8eeda664f
jianhua yao <jianhua.yao.us@gmail.com>
2014-12-09 09:04:30 -0500

    add database support

--- crawler/src/jhyao/crawler/Main.java
+++ crawler/src/jhyao/crawler/Main.java
@@ -1,6 +1,9 @@
 package jhyao.crawler;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,12 +15,19 @@ import org.jsoup.select.Elements;
 public class Main {
 	private int count;//number of books to be stored
 	private Set<String> visited;//visited urls
+	private static Database db = new Database();
 	
 	public static void main(String[] args){
 		//entry url
 		String host = "http://newyork.craigslist.org/search/bka";
 		int count = 1000;
 		Main crawler = new Main(count);
+		try {
+			//delete all records initially
+			db.execute("TRUNCATE books;");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		crawler.processPage(host);
 	}
 	
@@ -37,6 +47,8 @@ public class Main {
 			Elements books = doc.select("span[class=txt]");
 			Elements titles = null;
 			Elements prices = null;
+			String sql = "insert into `crawler`.`books` (`title`,`price`) values (?,?)";
+			PreparedStatement stmt = db.conn.prepareStatement(sql);
 			for(Element book: books){
 				//task completed and return
 				if(count==0) return;
@@ -52,7 +64,10 @@ public class Main {
 				prices = book.select("span[class=price]");
 				if(!prices.isEmpty()) price = Double.parseDouble(prices.first().text().substring(1));
 				
-				System.out.println(count+":"+title+" "+price);
+				//insert into database
+				stmt.setString(1, title);
+				stmt.setDouble(2, price);
+				stmt.execute();
 			}
 				
 			//get next page
@@ -67,7 +82,7 @@ public class Main {
 					processPage(next);
 				}
 			}
-		} catch (IOException | InterruptedException e) {
+		} catch (IOException | SQLException | InterruptedException e) {
 			e.printStackTrace();
 		}
 		
